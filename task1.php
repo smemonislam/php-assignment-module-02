258f1e832e62ead393f6dc2a9bc3a53a32fd4569
SM Emon Islam <73327452+smemonislam@users.noreply.github.com>
2023-09-19 21:10:22 +0600

    Added New Idea

--- task1.php
+++ task1.php
@@ -37,6 +37,14 @@ printEvenNumbers(1, 20, 2);
 
 // printEvenNumbersUsingForLoop(1, 20, 2);
 
+// function isEven($start, $end, $step)
+// {
+//     for ($i = $start; $i <= $end; $i += $step) {
+//         echo $i + 1 . " ";
+//     }
+// }
+// isEven(1, 20, 2);
+
 
 /**
  * Using a while loop
