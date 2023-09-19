53a021d78ebd203b880931d7d2b5d7772d0d6337
SM Emon Islam <73327452+smemonislam@users.noreply.github.com>
2023-09-19 16:48:35 +0600

    condition update

--- task1.php
+++ task1.php
@@ -5,13 +5,16 @@ function printEvenNumbersFor(int $start, int $end, int $step)
 {
     echo "Using for loop: ";
     for ($i = $start; $i <= $end; $i += $step) {
+        if ($i % 2 == 1) {
+            $i = 2;
+        }
         if ($i % 2 == 0) {
             echo $i . " ";
         }
     }
     echo "\n";
 }
-printEvenNumbersFor(2, 20, 2);
+printEvenNumbersFor(1, 20, 2);
 
 
 
@@ -22,13 +25,18 @@ function printEvenNumbersWhile(int $start, int $end, int $step)
 
     while ($start <= $end) {
 
-        echo $start . " ";
+        if ($start % 2 == 1) {
+            $start = 2;
+        }
+        if ($start % 2 == 0) {
+            echo $start . " ";
+        }
 
         $start += $step;
     }
     echo "\n";
 }
-printEvenNumbersWhile(2, 20, 2);
+printEvenNumbersWhile(1, 20, 2);
 
 
 // Using a do-while loop
@@ -36,7 +44,12 @@ function printEvenNumbersDoWhile($start, $end, $step)
 {
     echo "Using do-while loop: ";
     do {
-        echo $start . " ";
+        if ($start % 2 == 1) {
+            $start = 2;
+        }
+        if ($start % 2 == 0) {
+            echo $start . " ";
+        }
         $start += $step;
     } while ($start <= $end);
     echo "\n";
